7b5e2f3868d9b35ce1c089bc1381e8f5afebde51
Chris <eng.christhuku@gmail.com>
2022-05-10 21:22:04 +0300

    feat notes in a table

--- src/app/contactmanager/contactmanager.module.ts
+++ src/app/contactmanager/contactmanager.module.ts
@@ -11,6 +11,7 @@ import { MaterialModule } from '../shared/material.module';
 import { FormsModule } from '@angular/forms';
 import { UserService } from './services/user.service';
 import { HttpClientModule } from '@angular/common/http';
+import { NotesComponent } from './componets/notes/notes.component';
 
 const routes: Routes = [
   {
@@ -29,7 +30,8 @@ const routes: Routes = [
     ContactmanagerAppComponent,
     ToolbarComponent,
     MainContentComponent,
-    SidenavComponent    
+    SidenavComponent,
+    NotesComponent  
   ],
   providers:[
     UserService
